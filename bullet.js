a20e7fc99c5cca688cc0fdd03b53a1da1f285b05
cheesycows <42721186+cheesycows@users.noreply.github.com>
2018-08-26 20:25:30 +0100

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -18,7 +18,7 @@ var Bullet = function (world) {
 
     this.buffers = bulletBuffers;
     this.scale = 2;
-    this.life = 1000;
+    this.life = 200;
 }
 
 Bullet.prototype = Object.create(Sprite.prototype); 
