c74277909362fbff05beadd0cfd11d84347dd871
cheesycows <42721186+cheesycows@users.noreply.github.com>
2018-08-27 18:11:46 +0100

    Update ship.js

--- ship.js
+++ ship.js
@@ -6,7 +6,7 @@
 var shipBuffers = [];
 
 function shipCreate() {
-    var vertices = [[2, 0],
+    var vertices = [[6, 0],
                     [Math.cos(rad(140)), Math.sin(rad(140))],
                     [-0.3, 0],
                     [Math.cos(rad(220)), Math.sin(rad(220))]]
